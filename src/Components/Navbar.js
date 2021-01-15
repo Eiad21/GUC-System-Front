d48cb2d7f4ba08bb5816450ac301a0904e9161e8
kimo18 <64424003+kimo18@users.noreply.github.com>
2021-01-14 16:58:12 -0800

    fixes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -88,12 +88,9 @@ function Navbar(props) {
               <a style={(props.user.MemberRank=="coordinator"||props.user.MemberRank=="instructor"||props.user.MemberRank=="hod")?{display: 'block', cursor:'pointer'}:{display: 'none'}} className={NBclassNotClicked} text ="Replacement Requests" onClick={handleNavClick}>Replacement Requests</a>
               <a style={(props.user.MemberRank=="coordinator"||props.user.MemberRank=="instructor"||props.user.MemberRank=="hod")?{display: 'block', cursor:'pointer'}:{display: 'none'}} className={NBclassNotClicked} text ="Change Day off" onClick={handleNavClick}>Change Day off</a>
               <a style={(props.user.MemberRank=="coordinator"||props.user.MemberRank=="instructor"||props.user.MemberRank=="hod")?{display: 'block', cursor:'pointer'}:{display: 'none'}} className={NBclassNotClicked} text ="Leave Request" onClick={handleNavClick}>Leave Request</a>
-<<<<<<< HEAD
-              <a style={(props.user.MemberRank=="coordinator"||props.user.MemberRank=="instructor")?{display: 'block', cursor:'pointer'}:{display: 'none'}} className={NBclassNotClicked} text ="Requests" onClick={handleNavClick}>Requests</a>
               <a style={(props.user.MemberRank=="coordinator")?{display: 'block', cursor:'pointer'}:{display: 'none'}} className={NBclassNotClicked} text ="Manage Courses" onClick={handleNavClick}>Manage Courses</a>
-=======
+
               <a style={(props.user.MemberRank=="coordinator"||props.user.MemberRank=="instructor") || props.user.MemberRank=="hod"?{display: 'block', cursor:'pointer'}:{display: 'none'}} className={NBclassNotClicked} text ="Requests" onClick={handleNavClick}>Requests</a>
->>>>>>> 7d47c0449507ea4ae405274e33e7a4b58d7965f7
 
               {/* coordinator  carentials  */}
               <a style={(props.user.MemberRank=="coordinator")?{display: 'block', cursor:'pointer'}:{display: 'none'}} className={NBclassNotClicked} text ="Recieved Linking Requests" onClick={handleNavClick}>Recieved Linking Requests</a>
@@ -108,13 +105,8 @@ function Navbar(props) {
 
               <a style={(props.user.MemberRank=="hod")?{display: 'block', cursor:'pointer'}:{display: 'none'}}   onClick={handleNavClick} className={NBclassNotClicked}>View Entity Info</a>
 
-
-<<<<<<< HEAD
-              <Link style={(props.user.MemberRank=="instructor")?{display: 'block'}:{display: 'none'}} className={NBclassNotClicked} to="/MyCourses">Courses</Link>
               
-=======
               <Link style={(props.user.MemberRank!="hr" && props.user.MemberRank!="hod")?{display: 'block'}:{display: 'none'}} className={NBclassNotClicked} to="/MyCourses">Courses</Link>
->>>>>>> 7d47c0449507ea4ae405274e33e7a4b58d7965f7
               
 
 
